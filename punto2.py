0f93b510dea7dc794c272557483bb39962bd0cb3
Santiago Rojas <santiago.barreneche@correounivalle.edu.co>
2022-01-03 12:50:54 -0500

    Se encapsula el puntoa en su funcion y se utiliza el modulo util para encontrar el maximo

--- punto2.py
+++ punto2.py
@@ -1,20 +1,21 @@
 import KolmogorovSmirov
+import util
 
 #implemente un generador lineal congruente con a = 100, m= 165123, c = 1234 y x0 = 32 y genere 10000 números. Trabaje con una confianza del 5 %
 def generador():
-    a=100
-    m=165123
-    c=1234
-    x0=32
+    a = 100
+    m = 165123
+    c = 1234
+    x0 = 32
     numeros=[]
     for i in range(10000):
-        x0=(a*x0 + c)%m
+        x0 = (a*x0 + c)%m
         numeros.append(x0)
    # print(len(numeros),numeros)
     return numeros
 
-datos = generador()
-
 #a
-maximo = KolmogorovSmirov.max(datos)
-KolmogorovSmirov.kolmogorovTabla( list(map(lambda x: x/maximo, datos )) , 0.05)
+def puntoa():
+    datos = generador()
+    maximo = util.max(datos)
+    KolmogorovSmirov.kolmogorovTabla( list(map(lambda x: x/maximo, datos )) , 0.05)
